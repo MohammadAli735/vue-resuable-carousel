eaa17b9d5c0c34602b2e48310c2eb3f1fbab9c34
MohammadAli735 <mqurbani735@gmail.com>
2023-03-19 19:41:58 +0430

    cube swiper resuable carousel for vue js

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import Carousel from './components/Carousel.vue'
+const photots = [
+  "src/assets/1.jpg",
+  "src/assets/2.jpg",
+  "src/assets/3.jpg",
+  "src/assets/4.png",
+]
+// note in real world usage you may get all images with an api call 
+</script>
+
+
+<template>
+  <main>
+    <Carousel :photots="photots" />
+  </main>
+</template>
+
+<style >
+body {
+  background-color: rgba(177, 230, 248, 0.7);
+}
+</style>
---
+++ src/components/Carousel.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import { EffectCube, Pagination } from 'swiper'
+
+import 'swiper/css/effect-cube'
+import 'swiper/css/pagination'
+import "swiper/css"
+const props = defineProps({
+    photots: {
+        required: true
+    }
+})
+</script>
+
+<template>
+    <div class=" text-center">
+        <h1>a simple carousel for vue js with swiper</h1>
+        <Swiper :modules="[EffectCube, Pagination]" effect="cube" :cubeEffect="{
+            shadow: true,
+            slideShadows: true,
+            shadowOffset: 20,
+            shadowScale: 0.95
+        }" :grabCursor="true" :loop="true" :pagination="true">
+            <SwiperSlide v-for="photo in props.photots" :key="photo">
+                <img :src="`${photo}`" alt="">
+            </SwiperSlide>
+        </Swiper>
+    </div>
+</template>
+
+<style>
+.swiper {
+    width: 500px;
+    height: 500px;
+    overflow: hidden;
+}
+
+.swiper-slide img {
+    width: 100%;
+    height: 100%;
+}
+
+.text-center {
+    text-align: center;
+}
+</style>
